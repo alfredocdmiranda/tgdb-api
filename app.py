da34e7d5835679e7af8a9630c8bfffcbade517b4
Alfredo Miranda <alfredo.miranda@hp.com>
2016-06-05 12:21:39 -0300

    Added routes and endpoints

--- app.py
+++ app.py
@@ -32,7 +32,9 @@ class APIGameList(Resource):
             help = 'No game release date provided')
         self.parser.add_argument('players', type = int, default = 1)
         self.parser.add_argument('developer', type = str, default = "Unknown")
-        self.parser.add_argument('publisher', type = str, default = "Unknown")
+        self.parser.add_argument('publisher', type = int, default = 1)
+        self.parser.add_argument('genre', type = int, default = 1)
+        self.parser.add_argument('rating', type = int, default = 1)
         super(APIGameList, self).__init__()
 
     def get(self):
@@ -43,12 +45,102 @@ class APIGameList(Resource):
     def post(self):
         args = self.parser.parse_args()
         args['release_date'] = datetime.strptime(args['release_date'], "%d/%m/%Y").date()
+        args['publisher'] = Publisher.query.filter_by(id=args['publisher']).first()
+        args['genre'] = Genre.query.filter_by(id=args['genre']).first()
+        args['rating'] = Rating.query.filter_by(id=args['rating']).first()
         game = Game(**args)
         print(game)
         db.session.add(game)
         db.session.commit()
 
         return game.to_json(), 201
+
+class APIGenre(Resource):
+    def get(self, genre_id):
+        genre = Genre.query.filter_by(id=genre_id).first()
+        if not genre:
+            abort(404)
+
+        return genre.to_json()
+
+class APIGenreList(Resource):
+    def __init__(self):
+        self.parser = reqparse.RequestParser()
+        self.parser.add_argument('name', type = str, required = True,
+            help = 'No genre name provided')
+        super(APIGenreList, self).__init__()
+
+    def get(self):
+        genres = [ g.to_json() for g in Genre.query.all()]
+
+        return genres
+
+    def post(self):
+        args = self.parser.parse_args()
+        genre = Genre(**args)
+        print(genre)
+        db.session.add(genre)
+        db.session.commit()
+
+        return genre.to_json(), 201
+
+class APIPublisher(Resource):
+    def get(self, publisher_id):
+        publisher = Publisher.query.filter_by(id=publisher_id).first()
+        if not publisher:
+            abort(404)
+
+        return publisher.to_json()
+
+class APIPublisherList(Resource):
+    def __init__(self):
+        self.parser = reqparse.RequestParser()
+        self.parser.add_argument('name', type = str, required = True,
+            help = 'No publisher name provided')
+        super(APIPublisherList, self).__init__()
+
+    def get(self):
+        publishers = [ g.to_json() for g in Publisher.query.all()]
+
+        return publishers
+
+    def post(self):
+        args = self.parser.parse_args()
+        publisher = Publisher(**args)
+        print(publisher)
+        db.session.add(publisher)
+        db.session.commit()
+
+        return publisher.to_json(), 201
+
+class APIRating(Resource):
+    def get(self, rating_id):
+        rating = Rating.query.filter_by(id=rating_id).first()
+        if not rating:
+            abort(404)
+
+        return rating.to_json()
+
+class APIRatingList(Resource):
+    def __init__(self):
+        self.parser = reqparse.RequestParser()
+        self.parser.add_argument('name', type = str, required = True,
+            help = 'No rating name provided')
+        super(APIRatingList, self).__init__()
+
+    def get(self):
+        ratings = [ g.to_json() for g in Rating.query.all()]
+
+        return ratings
+
+    def post(self):
+        args = self.parser.parse_args()
+        rating = Rating(**args)
+        print(rating)
+        db.session.add(rating)
+        db.session.commit()
+
+        return rating.to_json(), 201
 
 class APIStatus(Resource):
     def get(self):
@@ -60,6 +152,12 @@ class APIStatus(Resource):
 api.add_resource(APIStatus, '/status', endpoint='status')
 api.add_resource(APIGameList, '/games', endpoint='games')
 api.add_resource(APIGame, '/games/<game_id>', endpoint='game')
+api.add_resource(APIGenreList, '/genres', endpoint='genres')
+api.add_resource(APIGenre, '/genres/<genre_id>', endpoint='genre')
+api.add_resource(APIPublisherList, '/publishers', endpoint='publishers')
+api.add_resource(APIPublisher, '/publishers/<publisher_id>', endpoint='publisher')
+api.add_resource(APIRatingList, '/ratings', endpoint='ratings')
+api.add_resource(APIRating, '/ratings/<rating_id>', endpoint='rating')
 
 if __name__ == '__main__':
     db.create_all()
